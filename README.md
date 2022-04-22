864b83536cf260274aeebea39b3c629a70aa76a0
wale-prog <olapetanwale@outlook.com>
2022-04-22 18:07:25 +0100

    Check for linter errors

--- README.md
+++ README.md
@@ -1,62 +1,57 @@
-![](https://img.shields.io/badge/Microverse-blueviolet)
+# Module one Capstone Project
 
-# Project Name
+> Project Description.
 
-> Description the project.
+> In this Capstone project, we were given  a design guide line to follow which was based on an art conference but I have personalized it i.e changed the content to that of a technical conference advocating for Renewable Power Generation that has zero net carbon emission. 
+> The webpage is responsive and compartible with screen sizes lower than 768px, then higher screen sizes.
 
 
 ## Built With
 
-- Major languages
-- Frameworks
-- Technologies used
+- HTML
+- CSS
+- JavaScript
+- Git & Github
 
-## Live Demo (if available)
-
-[Live Demo Link](https://livedemo.com)
+## Live Demo
+[Live Demo Link](https://wale-prog.github.io/Module1-Capstone-Project/)
 
 
 ## Getting Started
 
-**This is an example of how you may give instructions on setting up your project locally.**
-**Modify this file to match your project, remove sections that don't apply. For example: delete the testing section if the currect project doesn't require testing.**
+**This project is part of a training series to highlight the need for proper documentation, best practices, and building clean codes with html, css and Javascript.**
 
-
-To get a local copy up and running follow these simple example steps.
 
 ### Prerequisites
+- Have an IDE installed on your computer to be able to view the code locally.
+- Have a good working knowledge of Git and Github.
 
 ### Setup
+- Open a command line terminal (Git bash preferrably) and navigate to a directory where you would like to save the work folder using cd.
+- Clone the repository with git clone https://github.com/wale-prog/Module1-Capstone-Project.git
+- To get the cuurent working tree, git fetch --all, then checkout to capstone-html/css branch.
+- Finally, display the content of the index.html by running a live server or by just opening the file using a web browser.
 
 ### Install
+- IDE preferrably VScode
 
 ### Usage
-
-### Run tests
-
-### Deployment
+- For any individual who wishes to practice basic HTML,CSS and Javascript
+- For practice of HTML5, CSS and Jvascript technologies.
 
 
+## Author
 
-## Authors
+👤 **Wale Olapetan**
 
-👤 **Author1**
+- GitHub: [@wale-prog](https://github.com/wale-prog)
+- Twitter: [@Wale_Petan](https://twitter.com/Wale_Petan)
 
-- GitHub: [@githubhandle](https://github.com/githubhandle)
-- Twitter: [@twitterhandle](https://twitter.com/twitterhandle)
-- LinkedIn: [LinkedIn](https://linkedin.com/in/linkedinhandle)
-
-👤 **Author2**
-
-- GitHub: [@githubhandle](https://github.com/githubhandle)
-- Twitter: [@twitterhandle](https://twitter.com/twitterhandle)
-- LinkedIn: [LinkedIn](https://linkedin.com/in/linkedinhandle)
 
 ## 🤝 Contributing
-
 Contributions, issues, and feature requests are welcome!
 
-Feel free to check the [issues page](../../issues/).
+
 
 ## Show your support
 
@@ -64,10 +59,11 @@ Give a ⭐️ if you like this project!
 
 ## Acknowledgments
 
-- Hat tip to anyone whose code was used
-- Inspiration
-- etc
+- Hat tip to [Cindy Shin in Behance](https://www.behance.net/adagio07) for the original design and guidelines.
+- Shoutout to [@ijeomaodinko](https://github.com/ijeomaodinko) for being amazing.
+- Microverse.
 
 ## 📝 License
+
 
 This project is [MIT](./MIT.md) licensed.
--- index.js
+++ index.js
@@ -1,60 +1,60 @@
 const hamburger = document.querySelector('.hamburger');
 const popupMenu = document.querySelector('.menu');
 const closeBtn = document.querySelector('.close');
-const body = document.querySelector('body')
+const body = document.querySelector('body');
 
 hamburger.addEventListener('click', () => {
-    popupMenu.classList.add('active');
-    body.classList.add('scroll-lock')
+  popupMenu.classList.add('active');
+  body.classList.add('scroll-lock');
+  hamburger.classList.add('active');
 });
 
 closeBtn.addEventListener('click', () => {
-    popupMenu.classList.remove('active');
-    body.classList.remove('scroll-lock')
+  popupMenu.classList.remove('active');
+  body.classList.remove('scroll-lock');
+  hamburger.classList.remove('active');
 });
 
 const speakerSection = document.createElement('div');
 const speakers = document.getElementById('speakers');
 
-
-
 const featuredSpeaker = [
-    {
-        chessboard: 'images/chess-board.png',
-        speaker1Name: 'Keith Bell',
-        speaker1Title: '<i>Professor of Smart Grids, University of Strathclyde</i>',
-        speaker1Intro: 'Keith Bell holds the Scottish Power Chair in Future Power Systems at the University of Strathclyde. He joined the University in 2005 having previously worked as an electrical engineering researcher in Bath, Manchester and Naples, and as a system development engineer in the electricity supply industry in Britain.',
-        speaker1img: 'images/keith-bell.png',
+  {
+    chessboard: 'images/chess-board.png',
+    speaker1Name: 'Keith Bell',
+    speaker1Title: '<i>Professor of Smart Grids, University of Strathclyde</i>',
+    speaker1Intro: 'Keith Bell holds the Scottish Power Chair in Future Power Systems at the University of Strathclyde. He joined the University in 2005 having previously worked as an electrical engineering researcher in Bath, Manchester and Naples, and as a system development engineer in the electricity supply industry in Britain.',
+    speaker1img: 'images/keith-bell.png',
 
-        speaker2Name: 'Adam Berman',
-        speaker2Title: '<i>Deputy Director for Generation Policy, Energy UK</i>',
-        speaker2Intro: 'In this position, he advocates for ways to speed up the UK’s journey to Net-Zero through enabling low-carbon investment in clean power generation. Adam’s role is to ensure that Energy UK works closely with policymakers toward market-based policies that harness the power of the business        community to drive the energy transition.',
-        speaker2img: 'images/adam-berman.png',
+    speaker2Name: 'Adam Berman',
+    speaker2Title: '<i>Deputy Director for Generation Policy, Energy UK</i>',
+    speaker2Intro: 'In this position, he advocates for ways to speed up the UK’s journey to Net-Zero through enabling low-carbon investment in clean power generation. Adam’s role is to ensure that Energy UK works closely with policymakers toward market-based policies that harness the power of the business        community to drive the energy transition.',
+    speaker2img: 'images/adam-berman.png',
 
-        speaker3Name: 'Andrew Jamieson',
-        speaker3Title: '<i>Chief Executive, ORE Catapult</i>',
-        speaker3Intro: 'Andrew is Chief Executive of the Offshore Renewable Energy Catapult, a UK wide technology and innovation centre supporting businesses to accelerate the design, deployment and commercialisation of offshore renewable energy – wind, wave and tidal power. Jamieson brings over 40 years experience in the Energy sector.',
-        speaker3img: 'images/andrew-jamieson.png',
+    speaker3Name: 'Andrew Jamieson',
+    speaker3Title: '<i>Chief Executive, ORE Catapult</i>',
+    speaker3Intro: 'Andrew is Chief Executive of the Offshore Renewable Energy Catapult, a UK wide technology and innovation centre supporting businesses to accelerate the design, deployment and commercialisation of offshore renewable energy – wind, wave and tidal power. Jamieson brings over 40 years experience in the Energy sector.',
+    speaker3img: 'images/andrew-jamieson.png',
 
-        speaker4Name: 'Maria Kalogera',
-        speaker4Title: '<i>Innovations Manager, CrossWind</i>',
-        speaker4Intro: 'Renewable Energies Specialist with an Electrical Engineering background (MSc in Power Systems and Sustainable Energy Technologies from TU Delft), comprehensive project experience in offshore wind projects (Borssele 3,4 and CrossWind) and in mega projects (Prelude and Browse FLNG) and an insatiable curiosity for emerging technologies, energy transition and finding solutions to tackle energy resource scarcity.',
-        speaker4img: 'images/maria-kalogera.png',
+    speaker4Name: 'Maria Kalogera',
+    speaker4Title: '<i>Innovations Manager, CrossWind</i>',
+    speaker4Intro: 'Renewable Energies Specialist with an Electrical Engineering background (MSc in Power Systems and Sustainable Energy Technologies from TU Delft), comprehensive project experience in offshore wind projects (Borssele 3,4 and CrossWind) and in mega projects (Prelude and Browse FLNG) and an insatiable curiosity for emerging technologies, energy transition and finding solutions to tackle energy resource scarcity.',
+    speaker4img: 'images/maria-kalogera.png',
 
-        speaker5Name: 'Jon Gluyas',
-        speaker5Title: '<i>Chair in Geoenergy, Carbon Capture & Storage, Orsted</i>',
-        speaker5Intro: 'His current research examines the utility of abandoned and late-life petroleum fields for  supplying geothermal energy as well as the possibility that deep karst in Carboniferous limestones could also make major contribution the UK’s heating requirements as well as reducing the national carbon footprint.',
-        speaker5img: 'images/jon-glyus.png',
+    speaker5Name: 'Jon Gluyas',
+    speaker5Title: '<i>Chair in Geoenergy, Carbon Capture & Storage, Orsted</i>',
+    speaker5Intro: 'His current research examines the utility of abandoned and late-life petroleum fields for  supplying geothermal energy as well as the possibility that deep karst in Carboniferous limestones could also make major contribution the UK’s heating requirements as well as reducing the national carbon footprint.',
+    speaker5img: 'images/jon-glyus.png',
 
-        speaker6Name: 'Julian Leslie',
-        speaker6Title: '<i>Head of Networks & Chief Engineer, National Grid ESO</i>',
-        speaker6Intro: 'Responsible for identifying the future GB transmission network capability requirements and then making economic recommendations as to which options to take forward.',
-        speaker6img: 'images/julian-leslie.png'
-    },
-]
+    speaker6Name: 'Julian Leslie',
+    speaker6Title: '<i>Head of Networks & Chief Engineer, National Grid ESO</i>',
+    speaker6Intro: 'Responsible for identifying the future GB transmission network capability requirements and then making economic recommendations as to which options to take forward.',
+    speaker6img: 'images/julian-leslie.png',
+  },
+];
 
 featuredSpeaker.forEach((data) => {
-    speakerSection.innerHTML = `
+  speakerSection.innerHTML = `
     <h3> Featured Speakers</h3>
     <div class="line"></div>
     <div class="speakers container">
@@ -132,7 +132,7 @@ featuredSpeaker.forEach((data) => {
 
       <div class="more hidden-desktop">MORE</div>
     </div>
-    `
+    `;
 
-    speakers.appendChild(speakerSection)
-})
+  speakers.appendChild(speakerSection);
+});
